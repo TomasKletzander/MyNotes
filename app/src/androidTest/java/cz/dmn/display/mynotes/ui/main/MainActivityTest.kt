4370cbbe4c766edb8390cf4a9f7cca318cd34dbe
Tomáš Kletzander <kletzander@ginasystem.com>
2019-11-20 09:19:17 +0100

    Added instrumented UI tests

---
+++ app/src/androidTest/java/cz/dmn/display/mynotes/ui/main/MainActivityTest.kt
@@ -0,0 +1,28 @@
+package cz.dmn.display.mynotes.ui.main
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val rule = ActivityTestR
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onNoteClick() {
+    }
+}
